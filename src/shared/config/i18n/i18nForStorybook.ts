21adee2299e8e34286297006dbe2b0c7c3ecd684
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2023-03-08 18:58:43 +0300

    fixed error in CI

--- src/shared/config/i18n/i18nForStorybook.ts
+++ src/shared/config/i18n/i18nForStorybook.ts
@@ -4,16 +4,35 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const ns = ['translation', 'main', 'about'];
+const supportedLngs = ['en', 'ru'];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const resources = ns.reduce((acc: { [key: string]: any }, n) => {
+  supportedLngs.forEach((lng) => {
+    if (!acc[lng]) acc[lng] = {};
+    acc[lng] = {
+      ...acc[lng],
+      // eslint-disable-next-line import/no-dynamic-require, global-require
+      [n]: require(`../../../../public/locales/${lng}/${n}.json`),
+    };
+  });
+  return acc;
+}, {});
+
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(Backend)
   .init({
+    lng: 'en',
     fallbackLng: 'en',
-    debug: false,
-    interpolation: {
-      escapeValue: false,
-    },
+    defaultNS: 'common',
+    ns,
+    interpolation: { escapeValue: false },
+    react: { useSuspense: false },
+    supportedLngs,
+    resources,
   });
 
 export default i18n;
