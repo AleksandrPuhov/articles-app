91106a59149e436f52e35184d7b9108ce4cf49b7
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2023-11-07 00:55:50 +0300

    add json server

--- scripts/generate-visual-json-report.js
+++ scripts/generate-visual-json-report.js
@@ -1,21 +1,22 @@
+/* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { promisify } = require('util');
-const { readdir, writeFile } = require('fs');
-const { join: joinPath, relative } = require('path');
+const { promisify } = require("util");
+const { readdir, writeFile } = require("fs");
+const { join: joinPath, relative } = require("path");
 
 const asyncReaddir = promisify(readdir);
 const writeFileAsync = promisify(writeFile);
 
-const lokiDir = joinPath(__dirname, '..', '.loki');
-const actualDir = joinPath(lokiDir, 'current');
-const expectedDir = joinPath(lokiDir, 'reference');
-const diffDir = joinPath(lokiDir, 'difference');
+const lokiDir = joinPath(__dirname, "..", ".loki");
+const actualDir = joinPath(lokiDir, "current");
+const expectedDir = joinPath(lokiDir, "reference");
+const diffDir = joinPath(lokiDir, "difference");
 
 (async function main() {
   const diffs = await asyncReaddir(diffDir);
 
   await writeFileAsync(
-    joinPath(lokiDir, 'report.json'),
+    joinPath(lokiDir, "report.json"),
     JSON.stringify({
       newItems: [],
       deletedItems: [],
